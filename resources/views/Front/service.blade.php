02ed46f5865c7b6cab7654a8105d1c089bf04545
Baraaabudraz <abudrazbaraa@gmail.com>
2024-07-21 01:59:30 +0300

    create service blade php

---
+++ resources/views/Front/service.blade.php
@@ -0,0 +1,108 @@
+@extends('Front.parent')
+@section('title','Services')
+@section('content')
+<!-- Service Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <h6 class="section-title bg-white text-center text-primary px-3">Services</h6>
+            <h1 class="display-6 mb-4">We Focuse On Making The Best In All Sectors</h1>
+        </div>
+        <div class="row g-4">
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                <a class="service-item d-block rounded text-center h-100 p-4" href="">
+                    <img class="img-fluid rounded mb-4" src="/uplancer/img/service-1.jpg" alt="">
+                    <h4 class="mb-0">Web Development</h4>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                <a class="service-item d-block rounded text-center h-100 p-4" href="">
+                    <img class="img-fluid rounded mb-4" src="/uplancer/img/service-2.jpg" alt="">
+                    <h4 class="mb-0">Mobile App Development</h4>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                <a class="service-item d-block rounded text-center h-100 p-4" href="">
+                    <img class="img-fluid rounded mb-4" src="/uplancer/img/service-3.jpg" alt="">
+                    <h4 class="mb-0">SEO</h4>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                <a class="service-item d-block rounded text-center h-100 p-4" href="">
+                    <img class="img-fluid rounded mb-4" src="/uplancer/img/service-4.jpg" alt="">
+                    <h4 class="mb-0">Social Media Marketing</h4>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                <a class="service-item d-block rounded text-center h-100 p-4" href="">
+                    <img class="img-fluid rounded mb-4" src="/uplancer/img/service-5.jpg" alt="">
+                    <h4 class="mb-0">Graphic Design</h4>
+                </a>
+            </div>
+            <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+                <a class="service-item d-block rounded text-center h-100 p-4" href="">
+                    <img class="img-fluid rounded mb-4" src="/uplancer/img/service-6.jpg" alt="">
+                    <h4 class="mb-0">UI/UX Design</h4>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Service End -->
+
+
+<!-- Testimonial Start -->
+<!--<div class="container-xxl py-5">-->
+<!--    <div class="container">-->
+<!--        <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">-->
+<!--            <h6 class="section-title bg-white text-center text-primary px-3">Testimonial</h6>-->
+<!--            <h1 class="display-6 mb-4">What Our Clients Say!</h1>-->
+<!--        </div>-->
+<!--        <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">-->
+<!--            <div class="testimonial-item bg-light rounded p-4">-->
+<!--                <div class="d-flex align-items-center mb-4">-->
+<!--                    <img class="flex-shrink-0 rounded-circle border p-1" src="/uplancer/img/testimonial-1.jpg" alt="">-->
+<!--                    <div class="ms-4">-->
+<!--                        <h5 class="mb-1">Client Name</h5>-->
+<!--                        <span>Profession</span>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>-->
+<!--            </div>-->
+<!--            <div class="testimonial-item bg-light rounded p-4">-->
+<!--                <div class="d-flex align-items-center mb-4">-->
+<!--                    <img class="flex-shrink-0 rounded-circle border p-1" src="/uplancer/img/testimonial-2.jpg" alt="">-->
+<!--                    <div class="ms-4">-->
+<!--                        <h5 class="mb-1">Client Name</h5>-->
+<!--                        <span>Profession</span>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>-->
+<!--            </div>-->
+<!--            <div class="testimonial-item bg-light rounded p-4">-->
+<!--                <div class="d-flex align-items-center mb-4">-->
+<!--                    <img class="flex-shrink-0 rounded-circle border p-1" src="/uplancer/img/testimonial-3.jpg" alt="">-->
+<!--                    <div class="ms-4">-->
+<!--                        <h5 class="mb-1">Client Name</h5>-->
+<!--                        <span>Profession</span>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>-->
+<!--            </div>-->
+<!--            <div class="testimonial-item bg-light rounded p-4">-->
+<!--                <div class="d-flex align-items-center mb-4">-->
+<!--                    <img class="flex-shrink-0 rounded-circle border p-1" src="/uplancer/img/testimonial-4.jpg" alt="">-->
+<!--                    <div class="ms-4">-->
+<!--                        <h5 class="mb-1">Client Name</h5>-->
+<!--                        <span>Profession</span>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+<!-- Testimonial End -->
+
+
+@endsection
