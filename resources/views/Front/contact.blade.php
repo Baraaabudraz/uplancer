cf2d41492c9d831f8578def93610478184a6443b
Baraaabudraz <abudrazbaraa@gmail.com>
2024-07-21 02:04:46 +0300

    create contact blade php

---
+++ resources/views/Front/contact.blade.php
@@ -0,0 +1,60 @@
+@extends('Front.parent')
+@section('content')
+    <!-- Contact Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                <h6 class="section-title bg-white text-center text-primary px-3">Contact Us</h6>
+                <h1 class="display-6 mb-4">If You Have Any Query, Please Feel Free Contact Us</h1>
+            </div>
+            <div class="row g-0 justify-content-center">
+                <div class="col-lg-8 wow fadeInUp" data-wow-delay="0.5s">
+                    <p class="text-center mb-4">Contact us now and experience the exceptional service you deserve. Our professional team is ready to listen to your needs and provide innovative solutions that ensure your success. Call us today and discover how we can help you achieve your goals through our strong collaboration and unique client experience.</p>
+<!--                    <form>-->
+<!--                        <div class="row g-3">-->
+<!--                            <div class="col-md-6">-->
+<!--                                <div class="form-floating">-->
+<!--                                    <input type="text" class="form-control" id="name" placeholder="Your Name">-->
+<!--                                    <label for="name">Your Name</label>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="col-md-6">-->
+<!--                                <div class="form-floating">-->
+<!--                                    <input type="email" class="form-control" id="email" placeholder="Your Email">-->
+<!--                                    <label for="email">Your Email</label>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="col-12">-->
+<!--                                <div class="form-floating">-->
+<!--                                    <input type="text" class="form-control" id="subject" placeholder="Subject">-->
+<!--                                    <label for="subject">Subject</label>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            <div class="col-12">-->
+<!--                                <div class="form-floating">-->
+<!--                                    <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 200px"></textarea>-->
+<!--                                    <label for="message">Message</label>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                         -->
+<!--                        </div>-->
+<!--                    </form>-->
+                    <div class="col-12 text-center">
+                        <a class="btn btn-primary rounded-pill py-3 px-5" type="submit" href="https://wa.me/+970597644664" target="_blank">Contact us</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
+
+
+    <!-- Google Map Start -->
+<!--    <div class="container-xxl pt-5 px-0 wow fadeIn" data-wow-delay="0.1s">-->
+<!--        <iframe class="w-100 mb-n2" style="height: 450px;"-->
+<!--            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"-->
+<!--            frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>-->
+<!--    </div>-->
+    <!-- Google Map End -->
+
+@endsection
