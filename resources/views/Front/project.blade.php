7fe6c801ea30e59cfeee78d511b3d391ec21f74b
Baraaabudraz <abudrazbaraa@gmail.com>
2024-07-21 02:02:01 +0300

    create project blade php
    Co-Authored-By: Baraaabudraz <102161963+baraaabudraz@users.noreply.github.com>

---
+++ resources/views/Front/project.blade.php
@@ -0,0 +1,127 @@
+@extends('Front.parent')
+@section('title','Project')
+@section('content')
+<!-- Project Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+            <h6 class="section-title bg-white text-center text-primary px-3">Our Projects</h6>
+            <h1 class="display-6 mb-4">Learn More About Our Complete Projects</h1>
+        </div>
+        <div class="owl-carousel project-carousel wow fadeInUp" data-wow-delay="0.1s">
+            <div class="project-item border rounded h-100 p-4" data-dot="01" style="height: 80px">
+                <div class="position-relative mb-4">
+                    <img class="img-fluid rounded" src="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.01.32.jpg" alt="">
+                    <a href="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.01.32.jpg" data-lightbox="project"><i class="fa fa-eye fa-2x"></i></a>
+                </div>
+                <h6>Elektra Store (Mobile App)</h6>
+                <span>Thousands of products with free home delivery.</span>
+            </div>
+            <div class="project-item border rounded h-100 p-4" data-dot="02">
+                <div class="position-relative mb-4">
+                    <img class="img-fluid rounded" src="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.01.51.jpg" alt="">
+                    <a href="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.01.51.jpg" data-lightbox="project"><i class="fa fa-eye fa-2x"></i></a>
+                </div>
+                <h6>Budget Planner (Mobile App)</h6>
+                <span>App It allows you to create a personalized monthly budget that helps you control your finances.</span>
+            </div>
+            <div class="project-item border rounded h-100 p-4" data-dot="03">
+                <div class="position-relative mb-4">
+                    <img class="img-fluid rounded" src="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.02.12.jpg" alt="">
+                    <a href="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.02.12.jpg" data-lightbox="project"><i class="fa fa-eye fa-2x"></i></a>
+                </div>
+                <h6>B Store</h6>
+                <span>An online store application to purchase products.</span>
+            </div>
+            <div class="project-item border rounded h-100 p-4" data-dot="04">
+                <div class="position-relative mb-4">
+                    <img class="img-fluid rounded" src="/uplancer/portfolio/seo/2022-12-30_17h07_15.png" alt="">
+                    <a href="/uplancer/portfolio/seo/2022-12-30_17h07_15.png" data-lightbox="project"><i class="fa fa-eye fa-2x"></i></a>
+                </div>
+                <h6>On-page and Technical SEO</h6>
+                <span>In the past 28 days, we successfully improved the number of organic clicks and impressions for an ecommerce store.</span>
+            </div>
+            <div class="project-item border rounded h-100 p-4" data-dot="05">
+                <div class="position-relative mb-4">
+                    <img class="img-fluid rounded" src="/uplancer/portfolio/seo/2023-01-13_21h22_33.png" alt="">
+                    <a href="/uplancer/portfolio/seo/2023-01-13_21h22_33.png" data-lightbox="project"><i class="fa fa-eye fa-2x"></i></a>
+                </div>
+                <h6>Keyword Research and On-page SEO for Ecommerce Stores</h6>
+                <span>In the past 28 days, we achieved remarkable results for an ecommerce store by increasing the total revenue by 278.6% and boosting the conversion rate by 49.5%.</span>
+            </div>
+            <div class="project-item border rounded h-100 p-4" data-dot="08">
+                <div class="position-relative mb-4">
+                    <img class="img-fluid rounded" src="/uplancer/portfolio/Web/EMS.jpg" alt="">
+                    <a href="/uplancer/portfolio/Web/EMS%20Dashboard.png" data-lightbox="project"><i class="fa fa-eye fa-2x"></i></a>
+                </div>
+                <h6>Employee Management System (Web App)</h6>
+                <span>Web system for Employee Management</span>
+            </div>
+            <div class="project-item border rounded h-100 p-4" data-dot="09">
+                <div class="position-relative mb-4">
+                    <img class="img-fluid rounded" src="/uplancer/img/project-9.jpg" alt="">
+                    <a href="/uplancer/img/project-9.jpg" data-lightbox="project"><i class="fa fa-eye fa-2x"></i></a>
+                </div>
+                <h6>UI / UX Design</h6>
+                <span>Digital agency website design and development</span>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- Project End -->
+
+
+    <!-- Testimonial Start -->
+<!--    <div class="container-xxl py-5">-->
+<!--        <div class="container">-->
+<!--            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">-->
+<!--                <h6 class="section-title bg-white text-center text-primary px-3">Testimonial</h6>-->
+<!--                <h1 class="display-6 mb-4">What Our Clients Say!</h1>-->
+<!--            </div>-->
+<!--            <div class="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">-->
+<!--                <div class="testimonial-item bg-light rounded p-4">-->
+<!--                    <div class="d-flex align-items-center mb-4">-->
+<!--                        <img class="flex-shrink-0 rounded-circle border p-1" src="/uplancer/img/testimonial-1.jpg" alt="">-->
+<!--                        <div class="ms-4">-->
+<!--                            <h5 class="mb-1">Client Name</h5>-->
+<!--                            <span>Profession</span>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>-->
+<!--                </div>-->
+<!--                <div class="testimonial-item bg-light rounded p-4">-->
+<!--                    <div class="d-flex align-items-center mb-4">-->
+<!--                        <img class="flex-shrink-0 rounded-circle border p-1" src="/uplancer/img/testimonial-2.jpg" alt="">-->
+<!--                        <div class="ms-4">-->
+<!--                            <h5 class="mb-1">Client Name</h5>-->
+<!--                            <span>Profession</span>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>-->
+<!--                </div>-->
+<!--                <div class="testimonial-item bg-light rounded p-4">-->
+<!--                    <div class="d-flex align-items-center mb-4">-->
+<!--                        <img class="flex-shrink-0 rounded-circle border p-1" src="/uplancer/img/testimonial-3.jpg" alt="">-->
+<!--                        <div class="ms-4">-->
+<!--                            <h5 class="mb-1">Client Name</h5>-->
+<!--                            <span>Profession</span>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>-->
+<!--                </div>-->
+<!--                <div class="testimonial-item bg-light rounded p-4">-->
+<!--                    <div class="d-flex align-items-center mb-4">-->
+<!--                        <img class="flex-shrink-0 rounded-circle border p-1" src="/uplancer/img/testimonial-4.jpg" alt="">-->
+<!--                        <div class="ms-4">-->
+<!--                            <h5 class="mb-1">Client Name</h5>-->
+<!--                            <span>Profession</span>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                    <p class="mb-0">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit diam amet diam et eos. Clita erat ipsum et lorem et sit.</p>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+    <!-- Testimonial End -->
+@endsection
