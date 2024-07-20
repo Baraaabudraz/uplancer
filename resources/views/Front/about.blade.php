f4fe28332bd6c1f96313f0574906712eb99c8227
Baraaabudraz <abudrazbaraa@gmail.com>
2024-07-21 02:04:56 +0300

    update about

--- resources/views/Front/about.blade.php
+++ resources/views/Front/about.blade.php
@@ -1,151 +1,156 @@
 @extends('Front.parent')
 @section('content')
-    <!-- About Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="row g-5">
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="img-border">
-                        <img class="img-fluid" src="img/about.jpg" alt="">
-                    </div>
+<!-- About Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="img-border">
+                    <img class="img-fluid" src="/uplancer/img/about.jpg" alt="">
                 </div>
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="h-100">
-                        <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
-                        <h1 class="display-6 mb-4">#1 Digital Solution With <span class="text-primary">10 Years</span> Of Experience</h1>
-                        <p>Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                        <p class="mb-4">Stet no et lorem dolor et diam, amet duo ut dolore vero eos. No stet est diam rebum amet diam ipsum. Clita clita labore, dolor duo nonumy clita sit at, sed sit sanctus dolor eos.</p>
-                        <div class="d-flex align-items-center mb-4 pb-2">
-                            <img class="flex-shrink-0 rounded-circle" src="img/team-1.jpg" alt="" style="width: 50px; height: 50px;">
-                            <div class="ps-4">
-                                <h6>Jhon Doe</h6>
-                                <small>SEO & Founder</small>
-                            </div>
-                        </div>
-                        <a class="btn btn-primary rounded-pill py-3 px-5" href="">Read More</a>
-                    </div>
+            </div>
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="h-100">
+                    <h6 class="section-title bg-white text-start text-primary pe-3">About Us</h6>
+
+                    <h1 class="display-6 mb-4"># Empower Your Success with <span class="text-primary">Up Lancer</span>- Where Excellence Meets Innovation </h1>
+                    <p>Welcome to UP Lancer, the place where creativity and technological advancements converge. Allow us to assist you in realizing your vision and transforming your ideas into reality through our specialized team in web and mobile development, user interface design, and marketing. Join us and embark on a distinctive journey of programming, design, and marketing that will propel you to new heights.</p>
+                    <!--                        <p class="mb-4"></p>-->
+                    <!--                        <div class="d-flex align-items-center mb-4 pb-2">-->
+                    <!--                            <img class="flex-shrink-0 " src="/uplancer/logo/up-lancer-team-logo.png" alt="" style="width: 80px; height: 50px;">-->
+                    <!--&lt;!&ndash;                            <div class="ps-4">&ndash;&gt;-->
+                    <!--&lt;!&ndash;                                <h6>Jhon Doe</h6>&ndash;&gt;-->
+                    <!--&lt;!&ndash;                                <small>SEO & Founder</small>&ndash;&gt;-->
+                    <!--&lt;!&ndash;                            </div>&ndash;&gt;-->
+                    <!--                        </div>-->
+                    <!--                        <a class="btn btn-primary rounded-pill py-3 px-5" href="">Read More</a>-->
                 </div>
             </div>
         </div>
     </div>
-    <!-- About End -->
+</div>
+<!-- About End -->
 
 
-    <!-- Feature Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="row g-5">
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="h-100">
-                        <h6 class="section-title bg-white text-start text-primary pe-3">Why Choose Us</h6>
-                        <h1 class="display-6 mb-4">Why People Trust Us? Learn About Us!</h1>
-                        <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
-                        <div class="row g-4">
-                            <div class="col-12">
-                                <div class="skill">
-                                    <div class="d-flex justify-content-between">
-                                        <p class="mb-2">Digital Marketing</p>
-                                        <p class="mb-2">85%</p>
-                                    </div>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
+<!-- Feature Start -->
+<div class="container-xxl py-5">
+    <div class="container">
+        <div class="row g-5">
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+                <div class="h-100">
+                    <h6 class="section-title bg-white text-start text-primary pe-3">Why Choose Us</h6>
+                    <h1 class="display-6 mb-4">Why People Trust Us?</h1>
+                    <p class="mb-4">At UP Lancer, we have earned the trust and confidence of our clients by consistently delivering excellent results and maintaining a strong track record of success. Here are the key factors that contribute to the trust our clients place in us:</p>
+                    <div class="row g-4">
+                        <div class="col-12">
+                            <div class="skill">
+                                <div class="justify-content-between">
+                                    <p class="mb-1">Expertise: </p>
+                                    <p class="mb-4">UP Lancer is home to a team of highly skilled professionals specializing in web development, mobile app development, UI/UX design, and digital marketing. Clients trust our team for their extensive knowledge and ability to provide exceptional solutions.</p>
                                 </div>
+                                <!--                                    <div class="progress">-->
+                                <!--                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>-->
+                                <!--                                    </div>-->
                             </div>
-                            <div class="col-12">
-                                <div class="skill">
-                                    <div class="d-flex justify-content-between">
-                                        <p class="mb-2">SEO & Backlinks</p>
-                                        <p class="mb-2">90%</p>
-                                    </div>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="skill">
+                                <div class="justify-content-between">
+                                    <p class="mb-1">Track Record:</p>
+                                    <p class="mb-2">We have a proven track record of completing projects on time and exceeding client expectations, showcasing our ability to consistently deliver high-quality outcomes.</p>
                                 </div>
+                                <!--                                    <div class="progress">-->
+                                <!--                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>-->
+                                <!--                                    </div>-->
                             </div>
-                            <div class="col-12">
-                                <div class="skill">
-                                    <div class="d-flex justify-content-between">
-                                        <p class="mb-2">Design & Development</p>
-                                        <p class="mb-2">95%</p>
-                                    </div>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="skill">
+                                <div class="justify-content-between">
+                                    <p class="mb-1">Client-Centric Approach: </p>
+                                    <p class="mb-2">We prioritize the needs and goals of our clients. We invest time in understanding their unique requirements and tailor our solutions accordingly. Our client-centric approach ensures personalized experiences and solutions that align precisely with their vision.</p>
                                 </div>
+                                <!--                                    <div class="progress">-->
+                                <!--                                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>-->
+                                <!--                                    </div>-->
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="img-border">
-                        <img class="img-fluid" src="img/feature.jpg" alt="">
-                    </div>
+            </div>
+            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+                <div class="img-border">
+                    <img class="img-fluid" src="/uplancer/img/feature.jpg" alt="">
                 </div>
             </div>
         </div>
     </div>
-    <!-- Feature End -->
+</div>
+<!-- Feature End -->
 
 
     <!-- Team Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-                <h6 class="section-title bg-white text-center text-primary px-3">Our Team</h6>
-                <h1 class="display-6 mb-4">We Are A Creative Team For Your Dream Project</h1>
-            </div>
-            <div class="row g-4">
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="team-item text-center p-4">
-                        <img class="img-fluid border rounded-circle w-75 p-2 mb-4" src="img/team-1.jpg" alt="">
-                        <div class="team-text">
-                            <div class="team-title">
-                                <h5>Full Name</h5>
-                                <span>Designation</span>
-                            </div>
-                            <div class="team-social">
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="team-item text-center p-4">
-                        <img class="img-fluid border rounded-circle w-75 p-2 mb-4" src="img/team-2.jpg" alt="">
-                        <div class="team-text">
-                            <div class="team-title">
-                                <h5>Full Name</h5>
-                                <span>Designation</span>
-                            </div>
-                            <div class="team-social">
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="team-item text-center p-4">
-                        <img class="img-fluid border rounded-circle w-75 p-2 mb-4" src="img/team-3.jpg" alt="">
-                        <div class="team-text">
-                            <div class="team-title">
-                                <h5>Full Name</h5>
-                                <span>Designation</span>
-                            </div>
-                            <div class="team-social">
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-twitter"></i></a>
-                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<!--    <div class="container-xxl py-5">-->
+<!--        <div class="container">-->
+<!--            <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">-->
+<!--                <h6 class="section-title bg-white text-center text-primary px-3">Our Team</h6>-->
+<!--                <h1 class="display-6 mb-4">We Are A Creative Team For Your Dream Project</h1>-->
+<!--            </div>-->
+<!--            <div class="row g-4">-->
+<!--                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">-->
+<!--                    <div class="team-item text-center p-4">-->
+<!--                        <img class="img-fluid border rounded-circle w-75 p-2 mb-4" src="/uplancer/img/team-1.jpg" alt="">-->
+<!--                        <div class="team-text">-->
+<!--                            <div class="team-title">-->
+<!--                                <h5>Baraa Abu Draz</h5>-->
+<!--                                <span>Web Developer</span>-->
+<!--                            </div>-->
+<!--                            <div class="team-social">-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-twitter"></i></a>-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">-->
+<!--                    <div class="team-item text-center p-4">-->
+<!--                        <img class="img-fluid border rounded-circle w-75 p-2 mb-4" src="/uplancer/img/team-2.jpg" alt="">-->
+<!--                        <div class="team-text">-->
+<!--                            <div class="team-title">-->
+<!--                                <h5>Full Name</h5>-->
+<!--                                <span>Designation</span>-->
+<!--                            </div>-->
+<!--                            <div class="team-social">-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-twitter"></i></a>-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">-->
+<!--                    <div class="team-item text-center p-4">-->
+<!--                        <img class="img-fluid border rounded-circle w-75 p-2 mb-4" src="/uplancer/img/team-3.jpg" alt="">-->
+<!--                        <div class="team-text">-->
+<!--                            <div class="team-title">-->
+<!--                                <h5>Full Name</h5>-->
+<!--                                <span>Designation</span>-->
+<!--                            </div>-->
+<!--                            <div class="team-social">-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-facebook-f"></i></a>-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-twitter"></i></a>-->
+<!--                                <a class="btn btn-square btn-primary rounded-circle" href=""><i class="fab fa-instagram"></i></a>-->
+<!--                            </div>-->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
     <!-- Team End -->
+
 @endsection
+
+
+
