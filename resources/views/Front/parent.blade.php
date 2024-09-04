41a05c89cb0b108eb906cac0220d15fbef862a19
Baraaabudraz <abudrazbaraa@gmail.com>
2024-09-04 12:32:12 +0300

    update parent for website

--- resources/views/Front/parent.blade.php
+++ resources/views/Front/parent.blade.php
@@ -152,77 +152,76 @@
 
 
 <!-- Footer Start -->
-<div class="container-fluid bg-dark text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s">
+<div class="container-fluid bg-dark text-body footer mt-5 pt-5 wow fadeIn" data-wow-delay="0.1s" style="visibility: visible; animation-delay: 0.1s; animation-name: fadeIn;">
     <div class="container py-5">
         <div class="row g-5">
             <div class="col-lg-3 col-md-6">
                 <h5 class="text-light mb-4">Address</h5>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>GAZA, PAL</p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+970597644664</p>
+                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>GAZA,PALESTINE</p>
+                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+970597644 664</p>
                 <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@uplancerps.com</p>
                 <div class="d-flex pt-2">
-                    <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-youtube"></i></a>
-                    <a class="btn btn-square btn-outline-secondary rounded-circle me-0" href=""><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href="https://twitter.com/uplancerps" target="_blank"><i class="fab fa-twitter"></i></a>
+                    <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href="https://www.facebook.com/uplancerps" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                    <!--                    <a class="btn btn-square btn-outline-secondary rounded-circle me-1" href=""><i class="fab fa-youtube"></i></a>-->
+                    <a class="btn btn-square btn-outline-secondary rounded-circle me-0" href="https://www.linkedin.com/company/uplancerps" target="_blank"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
             <div class="col-lg-3 col-md-6">
                 <h5 class="text-light mb-4">Quick Links</h5>
-                <a class="btn btn-link" href="">About Us</a>
-                <a class="btn btn-link" href="">Contact Us</a>
-                <a class="btn btn-link" href="">Our Services</a>
-                <a class="btn btn-link" href="">Terms & Condition</a>
-                <a class="btn btn-link" href="">Support</a>
+                <a class="btn btn-link" href="/about">About Us</a>
+                <a class="btn btn-link" href="/contact">Contact Us</a>
+                <a class="btn btn-link" href="/service">Our Services</a>
+                <a class="btn btn-link" href="#">Terms &amp; Condition</a>
+                <a class="btn btn-link" href="#">Support</a>
             </div>
             <div class="col-lg-3 col-md-6">
                 <h5 class="text-light mb-4">Gallery</h5>
                 <div class="row g-2">
                     <div class="col-4">
-                        <img class="img-fluid rounded" src="{{asset('uplancer/img/project-1.jpg')}}" alt="Image">
+                        <img class="img-fluid rounded" src="/uplancer/portfolio/Web/EMS.jpg" alt="Image">
                     </div>
                     <div class="col-4">
-                        <img class="img-fluid rounded" src="{{asset('uplancer/img/project-2.jpg')}}" alt="Image">
+                        <img class="img-fluid rounded" src="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.02.12.jpg" alt="Image">
                     </div>
                     <div class="col-4">
-                        <img class="img-fluid rounded" src="{{asset('uplancer/img/project-3.jpg')}}" alt="Image">
+                        <img class="img-fluid rounded" src="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.01.51.jpg" alt="Image">
                     </div>
                     <div class="col-4">
-                        <img class="img-fluid rounded" src="{{asset('uplancer/img/project-4.jpg')}}" alt="Image">
+                        <img class="img-fluid rounded" src="/uplancer/portfolio/mobile%20app/WhatsApp%20Image%202023-05-25%20at%2015.01.32.jpg" alt="Image">
                     </div>
                     <div class="col-4">
-                        <img class="img-fluid rounded" src="{{asset('uplancer/img/project-5.jpg')}}" alt="Image">
+                        <img class="img-fluid rounded" src="/uplancer/img/project-5.jpg" alt="Image">
                     </div>
                     <div class="col-4">
-                        <img class="img-fluid rounded" src="{{asset('uplancer/img/project-6.jpg')}}" alt="Image">
+                        <img class="img-fluid rounded" src="/uplancer/img/project-6.jpg" alt="Image">
                     </div>
                 </div>
             </div>
-{{--            <div class="col-lg-3 col-md-6">--}}
-{{--                <h5 class="text-light mb-4">Newsletter</h5>--}}
-{{--                <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>--}}
-{{--                <div class="position-relative mx-auto" style="max-width: 400px;">--}}
-{{--                    <input class="form-control bg-transparent border-secondary w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">--}}
-{{--                    <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+            <div class="col-lg-3 col-md-6">
+                <h5 class="text-light mb-4">Keep In Touch</h5>
+                <p>Tap into the power of effective communication by contacting us today. Together, we can transform your ideas into remarkable&nbsp;achievements</p>
+                <div class="position-relative mx-auto" style="max-width: 400px;">
+                    <!--                    <input class="form-control bg-transparent border-secondary w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">-->
+                    <a href="https://wa.me/+970597644664" type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2" target="_blank"><i class="fas fa-fw  fa-phone-alt me-2"></i>Contact Now</a>
+                </div>
+            </div>
         </div>
     </div>
     <div class="container-fluid copyright">
         <div class="container">
             <div class="row">
                 <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                    &copy; <a href="#">Up Lancer</a>, All Right Reserved.
+                    © <a href="#">Up Lancer</a>, All Right Reserved.
                 </div>
                 <div class="col-md-6 text-center text-md-end">
                     <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                    Designed By <a href="https://htmlcodex.com">Up Lancer</a>
+                    Designed By <a href="#">Up Lancer</a>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<!-- Footer End -->
+</div><!-- Footer End -->
 
 
 <!-- Back to Top -->
