53d59c19a591d1b9f534eaea14231d06480883a4
Baraaabudraz <abudrazbaraa@gmail.com>
2024-09-05 11:23:42 +0300

    Update login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Traits\AuthTrait;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -15,14 +13,13 @@ use Laravel\Socialite\Facades\Socialite;
 class LoginController extends Controller
 {
 
-    use AuthTrait;
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
-    public function loginForm($type){
-        return view('cms.user.auth.login',compact('type'));
-    }
+//    public function loginForm($type){
+//        return view('cms.user.auth.login',compact('type'));
+//    }
 
     public function login(Request $request)
     {
@@ -31,7 +28,7 @@ class LoginController extends Controller
             'password' => 'required'
         ]);
 
-        if (Auth::guard($this->checkGuard($request))->attempt(['email' => $request->email, 'password' => $request->password])) {
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password])) {
             return $this->redirect($request);
 
         }else{
@@ -41,46 +38,46 @@ class LoginController extends Controller
         }
 
     }
-    public function logout(Request $request,$type){
-        Auth::guard($type)->logout();
+    public function logout(Request $request){
+        Auth::guard('admin')->logout();
         $request->session()->invalidate();
-        return redirect()->route('loginView',$type);
+        return redirect()->route('loginView');
     }
 
-    public function redirectToProvider($driver)
-    {
-        return Socialite::driver($driver)->redirect();
-    }
+//    public function redirectToProvider($driver)
+//    {
+//        return Socialite::driver($driver)->redirect();
+//    }
 
-    public function handleProviderCallback($driver)
-    {
-        try {
-            $user = Socialite::driver($driver)->user();
-        } catch (\Exception $e) {
-            return redirect()->route('login');
-        }
-
-        $existingUser = User::where('email', $user->getEmail())->first();
-
-        if ($existingUser) {
-            auth()->guard('user')->login($existingUser, true);
-        } else {
-            $newUser                    = new User;
-            $newUser->provider_name     = $driver;
-            $newUser->provider_id       = $user->getId();
-            $newUser->name              = $user->getName();
-//            $newUser->user_name         = $user->getNickname();
-            $newUser->email             = $user->getEmail();
-            $newUser->password          = Hash::make($user->getName().'@'.$user->getId());
-            // we set email_verified_at because the user's email is already veridied by social login portal
-            $newUser->email_verified_at = now();
-            // you can also get avatar, so create avatar column in database it you want to save profile image
-            // $newUser->avatar            = $user->getAvatar();
-            $newUser->save();
-
-            auth()->guard('user')->login($newUser, true);
-        }
-
-        return redirect(route('home'));
-    }
+//    public function handleProviderCallback($driver)
+//    {
+//        try {
+//            $user = Socialite::driver($driver)->user();
+//        } catch (\Exception $e) {
+//            return redirect()->route('login');
+//        }
+//
+//        $existingUser = User::where('email', $user->getEmail())->first();
+//
+//        if ($existingUser) {
+//            auth()->guard('user')->login($existingUser, true);
+//        } else {
+//            $newUser                    = new User;
+//            $newUser->provider_name     = $driver;
+//            $newUser->provider_id       = $user->getId();
+//            $newUser->name              = $user->getName();
+////            $newUser->user_name         = $user->getNickname();
+//            $newUser->email             = $user->getEmail();
+//            $newUser->password          = Hash::make($user->getName().'@'.$user->getId());
+//            // we set email_verified_at because the user's email is already veridied by social login portal
+//            $newUser->email_verified_at = now();
+//            // you can also get avatar, so create avatar column in database it you want to save profile image
+//            // $newUser->avatar            = $user->getAvatar();
+//            $newUser->save();
+//
+//            auth()->guard('user')->login($newUser, true);
+//        }
+//
+//        return redirect(route('home'));
+//    }
 }
