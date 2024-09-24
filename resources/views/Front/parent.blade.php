1a00e8b03caf98d67500ee32f3d878ce5ace682b
Baraaabudraz <abudrazbaraa@gmail.com>
2024-09-24 20:16:57 +0300

    update script

--- resources/views/Front/parent.blade.php
+++ resources/views/Front/parent.blade.php
@@ -275,11 +275,11 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
 <script src="{{asset('uplancer/lib/wow/wow.min.js')}}"></script>
-<script src="{{asset('uplancer/lib/easing/easing.min.js')}}"></script>
-<script src="{{asset('uplancer/lib/waypoints/waypoints.min.js')}}"></script>
-<script src="{{asset('uplancer/lib/counterup/counterup.min.js')}}"></script>
-<script src="{{asset('uplancer/lib/owlcarousel/owl.carousel.min.js')}}"></script>
-<script src="{{asset('uplancer/lib/lightbox/js/lightbox.min.js')}}"></script>
+{{--<script src="{{asset('uplancer/lib/easing/easing.min.js')}}"></script>--}}
+{{--<script src="{{asset('uplancer/lib/waypoints/waypoints.min.js')}}"></script>--}}
+{{--<script src="{{asset('uplancer/lib/counterup/counterup.min.js')}}"></script>--}}
+{{--<script src="{{asset('uplancer/lib/owlcarousel/owl.carousel.min.js')}}"></script>--}}
+{{--<script src="{{asset('uplancer/lib/lightbox/js/lightbox.min.js')}}"></script>--}}
 
 <!-- Template Javascript -->
 <script src="{{asset('uplancer/js/main.js')}}"></script>
