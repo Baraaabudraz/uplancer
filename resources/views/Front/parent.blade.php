da2b32044da9f5de8853e5acb0856a2f6a125584
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-11-14 11:29:29 +0200

    Update parent.blade.php

--- resources/views/Front/parent.blade.php
+++ resources/views/Front/parent.blade.php
@@ -19,6 +19,8 @@
 {{--    <!-- Icon Font Stylesheet -->--}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
 
     @yield('styles')
 
