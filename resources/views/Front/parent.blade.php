cc1f5f6bde24b54a2138434f87239142e35f1f97
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-11-14 11:32:30 +0200

    Update parent.blade.php

--- resources/views/Front/parent.blade.php
+++ resources/views/Front/parent.blade.php
@@ -17,8 +17,8 @@
     <link href='https://fonts.googleapis.com/css?family=Cairo' rel='stylesheet'>
 
 {{--    <!-- Icon Font Stylesheet -->--}}
-{{--    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">--}}
-{{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">--}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 
