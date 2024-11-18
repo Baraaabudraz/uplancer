74571d93ad856beaa2cef4d995e1b0c4f323e041
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-11-18 14:09:28 +0200

    Update 2024_09_02_074312_create_services_table.php

--- database/migrations/2024_09_02_074312_create_services_table.php
+++ database/migrations/2024_09_02_074312_create_services_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->json('description');
             $table->string('image')->nullable();
             $table->string('icon');
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
