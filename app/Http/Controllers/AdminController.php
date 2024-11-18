9b550489cda4c12cd3c7c46a3ff939355c8e3a0a
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-11-18 14:09:26 +0200

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -82,12 +82,7 @@ class AdminController extends Controller
 
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
@@ -176,12 +171,7 @@ class AdminController extends Controller
 
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
