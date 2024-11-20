e50750df601ebeb1931389942c9323e95e958ca5
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2024-11-20 15:15:45 +0200

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Setting extends Model
 {
     use HasFactory;
+    use HasTranslations;
+    protected $translatable  = ['name'  , 'description','why_us','about' ,'desc_contact' ,'meta_description' ,'meta_keywords' ];
     protected $guarded=[];
 }
