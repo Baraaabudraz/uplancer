3b667d4f9bf80f9b0b92035441dbfd6f15818b36
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-09 15:28:18 +0200

    Update login.blade.php

--- resources/views/cms/login.blade.php
+++ resources/views/cms/login.blade.php
@@ -1,35 +1,19 @@
 <!DOCTYPE html>
-<!--
-Author: Keenthemes
-Product Name: Metronic - Bootstrap 5 HTML, VueJS, React, Angular & Laravel Admin Dashboard Theme
-Purchase: https://1.envato.market/EA4JP
-Website: http://www.keenthemes.com
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
-@if(App::getLocale() == 'ar')
-
-    <html lang="ar" dir="rtl">
-    @else
-        <html lang="en" dir="ltr">
-        @endif
+<html lang="{{App::getLocale() == 'ar' ? 'ar' : 'en' }}" dir="{{App::getLocale() == 'ar' ? 'rtl' : 'ltr' }}">
 <!--begin::Head-->
 <head><base href="../../../">
     <title>{{trans('dashboard_trans.Dashboard')}} | {{trans('dashboard_trans.Sign In')}}</title>
-    <meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
-    <meta name="keywords" content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charset="utf-8" />
-    <meta property="og:locale" content="en_US" />
+    <meta property="og:locale" content="ar_SA" />
     <meta property="og:type" content="article" />
-    <meta property="og:title" content="www.uplancerps.com" />
-    <meta property="og:url" content="www.uplancerps.com" />
+    <meta property="og:title" content="https://www.uplancerps.com" />
+    <meta property="og:url" content="https://www.uplancerps.com" />
     <meta property="og:site_name" content="Up Lancer" />
     <link rel="canonical" href="https://www.uplancerps.com" />
-    <link rel="shortcut icon" href="{{asset('admin/img/dashboard-logo.png')}}" />
+    <link rel="shortcut icon" href="{{asset('images/settings/favicon/'. setting()->favicon)}}" />
     <!--begin::Fonts-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
     <!--end::Fonts-->
@@ -47,14 +31,14 @@ License: For each use you must have a valid license purchased only from above li
 <!--begin::Body-->
 <body id="kt_body" class="bg-body">
 <!--begin::Main-->
-<div class="d-flex flex-column flex-root">
+<div class="d-flex flex-column flex-root" style="background-color: #5d3991">
     <!--begin::Authentication - Sign-in -->
-    <div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed" style="background-image: url(assets/media/illustrations/development-hd.png)">
+    <div class="d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed" >
         <!--begin::Content-->
         <div class="d-flex flex-center flex-column flex-column-fluid p-10 pb-lg-20">
             <!--begin::Logo-->
             <a href="{{route('home')}}" class="mb-12">
-                <img alt="Logo" src="{{asset('uplancer/logo/up-lancer-team-logo.png')}}" class="w-350px" />
+                <img alt="Up Lancer" src="{{asset('images/settings/logo/'.setting()->logo)}}" class="w-350px" />
             </a>
             <!--end::Logo-->
             <!--begin::Wrapper-->
@@ -105,8 +89,8 @@ License: For each use you must have a valid license purchased only from above li
                     <!--begin::Actions-->
                     <div class="text-center">
                         <!--begin::Submit button-->
-                        <button type="submit" class="btn btn-lg btn-primary w-100 mb-5">
-                            <span class="indicator-label" >{{trans('dashboard_trans.Sign In')}}</span>
+                        <button type="submit" class="btn btn-lg btn-primary w-100 mb-5" style="background-color: #5d3991">
+                            <span class="indicator-label">{{trans('dashboard_trans.Sign In')}}</span>
                         </button>
                         </div>
                         <!--end::Submit button-->
