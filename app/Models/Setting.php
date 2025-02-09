66a05b39b5b22841d46a49cafbcf48b13b2f963b
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-09 15:27:11 +0200

    Update Setting.php

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -10,6 +10,6 @@ class Setting extends Model
 {
     use HasFactory;
     use HasTranslations;
-    protected $translatable  = ['name'  , 'description','why_us','about' ,'desc_contact' ,'meta_description' ,'meta_keywords' ];
+    protected $translatable  = ['name', 'description','why_us','about' ,'desc_contact' ,'meta_description' ,'meta_keyword' , 'alt' ];
     protected $guarded=[];
 }
