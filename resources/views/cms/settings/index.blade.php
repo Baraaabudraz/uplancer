a10c4eb0ae71b197a587fb29a2facd4b0423d42b
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-09 15:29:03 +0200

    Update index.blade.php

--- resources/views/cms/settings/index.blade.php
+++ resources/views/cms/settings/index.blade.php
@@ -39,64 +39,41 @@
     </div>
     <!--end::Toolbar-->
     @if(session()->has('alert-type'))
-        <div class="alert {{session()->get('alert-type')}} alert-dismissible" role="alert">
-            {{session()->get('message')}}
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+        <div class="alert {{ session()->get('alert-type') }} alert-dismissible fade show" role="alert">
+            {{ session()->get('message') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
+
     @if($errors->any())
         @foreach($errors->all() as $error)
             <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                {{$error}}
-                <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                {{ $error }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endforeach
     @endif
-    <div class="card mb-5 mb-xl-10" id="kt_profile_details_view">
-        <!--begin::Card header-->
-
-        <div class="card-header cursor-pointer">
-            @if(!$website_settings)
-            <div class="card-toolbar">
-                <a href="{{route('settings.create')}}" class="btn btn-sm btn-light-primary">
-                    <!--begin::Svg Icon | path: icons/duotone/Communication/Add-user.svg-->
-                    <span class="svg-icon svg-icon-2">
-											<i class="fa fa-plus"></i>
-											</span>
-                    <!--end::Svg Icon-->  {{trans('dashboard_trans.Add Settings to website')}}</a>
-
-            </div>
-            @endif
-            <!--begin::Card title-->
-
-            <div class="card-title m-0">
-                <h3 class="fw-bolder m-0">{{trans('dashboard_trans.Website Settings')}}</h3>
-            </div>
-            <!--end::Card title-->
-            <!--begin::Action-->
-            @if($website_settings)
-                <a href="{{route('edit-website-settings')}}" class="btn btn-primary align-self-center">{{trans('dashboard_trans.Edit Website Settings')}}</a>
-            @endif
-
-            <!--end::Action-->
-        </div>
-        <!--begin::Card header-->
-        <!--begin::Card body-->
-        @if($website_settings)
-        <div class="card-body p-9">
-            <div class="col-lg-8 py-10">
-                <!--begin::Image input-->
-                <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url({{asset('assets/media/avatars/blank.png')}})">
-                    <!--begin::Preview existing avatar-->
-                    <div class="image-input-wrapper w-125px h-125px bgi-position-center" style="background-size: 100%; background-image: url({{url('images/settings/logo/',$website_settings->logo)}})"></div>
-                    <!--end::Preview existing avatar-->
+    <div id="kt_app_content" class="app-content flex-column-fluid">
+        <!--begin::Content container-->
+        <div id="kt_app_content_container" class="app-container container-xxl">
+            <div class="card shadow-sm">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <h3 class="mb-0">الموقع الإلكتروني</h3>
+                    @if(!$website_settings)
+                        <a href="{{ route('settings.create') }}" class="btn btn-primary">
+                            <i class="fa fa-plus"></i> ضبط اعداد الموقع
+                        </a>
+                    @endif
+                    @if($website_settings)
+                        <a href="{{ route('settings.edit',$website_settings->id) }}" class="btn btn-success">تعديل الإعدادات</a>
+                    @endif
                 </div>
-                <!--end::Image input-->
-            </div>
+
+                @if($website_settings)
+                    <div class="card-body">
+                        <div class="text-center mb-4" style="background-color: #5d3991">
+                            <img src="{{asset('images/settings/logo/'.$website_settings->logo)}}" class="image-input-wrapper w-200px  bgi-position-center mb-5"  alt="{{$website_settings->alt}}">
+                        </div>
             <!--begin::Row-->
             <div class="row mb-7">
                 <!--begin::Label-->
@@ -130,7 +107,7 @@
                 <!--begin::Col-->
                 <div class="col-lg-8 d-flex align-items-center">
                     <span class="fw-bolder fs-6 text-gray-800 me-2">{{$website_settings->phone}}</span>
-                    <span class="badge badge-success">Verified</span>
+                    <span class="badge badge-success">{{trans('dashboard_trans.Verified')}}</span>
                 </div>
                 <!--end::Col-->
             </div>
@@ -142,7 +119,7 @@
                 <!--end::Label-->
                 <!--begin::Col-->
                 <div class="col-lg-8">
-                    <a href="{{$website_settings->company_site}}" target="_blank" class="fw-bold fs-6 text-gray-800 text-hover-primary">{{$website_settings->company_site}}</a>
+                    <a href="{{$website_settings->url}}" target="_blank" class="fw-bold fs-6 text-gray-800 text-hover-primary">{{$website_settings->url}}</a>
                 </div>
                 <!--end::Col-->
             </div>
@@ -164,52 +141,19 @@
                 <!--end::Col-->
             </div>
             <!--end::Input group-->
-            <!--begin::Input group-->
-            <div class="row mb-10">
-                <!--begin::Label-->
-                <label class="col-lg-4 fw-bold text-muted">Allow Changes</label>
-                <!--begin::Label-->
-                <!--begin::Label-->
-                <div class="col-lg-8">
-                    <span class="fw-bold fs-6 text-gray-800">Yes</span>
-                </div>
-                <!--begin::Label-->
+                        <div class="card-footer text-center">
+                            <button onclick="confirmDelete(this, '{{ $website_settings->id }}')" class="btn btn-danger">{{trans('dashboard_trans.Delete')}}</button>
+                        </div>
+                        @else
+                            <div class="card-body text-center text-muted">لم يتم ضبط إعدادات الموقع بعد</div>
+                        @endif
+                    </div>
             </div>
-            <!--end::Input group-->
-            @else
-                No created Settings Yet
-            @endif
         </div>
-
-
-
-            <a href="#"  onclick="confirmDelete (this, '{{ $website_settings->id ?? '' }}')"
-               class="btn btn-sm btn-light-danger">
-                <!--begin::Svg Icon | path: icons/duotone/General/Trash.svg-->
-                <span class="svg-icon svg-icon-3">
-																	<svg xmlns="http://www.w3.org/2000/svg"
-                                                                         xmlns:xlink="http://www.w3.org/1999/xlink"
-                                                                         width="24px" height="24px" viewBox="0 0 24 24"
-                                                                         version="1.1">
-																		<g stroke="none" stroke-width="1" fill="none"
-                                                                           fill-rule="evenodd">
-																			<rect x="0" y="0" width="24" height="24"/>
-																			<path
-                                                                                d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z"
-                                                                                fill="#000000" fill-rule="nonzero"/>
-																			<path
-                                                                                d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z"
-                                                                                fill="#000000" opacity="0.3"/>
-																		</g>
-																	</svg>
-																</span>
-                <!--end::Svg Icon-->{{trans('dashboard_trans.Delete')}}
-            </a>
-        <!--end::Card body-->
-    </div>
-@endsection
-@section('scripts')
-        <script src="{{asset('/cms/assets/js/axios.js')}}"></script>
+        </div>
+        @endsection
+        @section('scripts')
+        <script src="{{asset('/assets/js/axios.js')}}"></script>
         <script>
             function confirmDelete(app, id) {
                 Swal.fire({
