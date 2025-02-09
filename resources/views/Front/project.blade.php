5fa64ca2f8500bf6f52683a0272f94d271f909aa
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-09 15:29:31 +0200

    Update project.blade.php

--- resources/views/Front/project.blade.php
+++ resources/views/Front/project.blade.php
@@ -72,7 +72,7 @@
                     <div class="col-md-4">
                         <div class="project-item shadow-lg rounded p-3 h-100">
                             <div class="position-relative mb-3">
-                                <img class="img-fluid rounded lazyload"   src="{{url('images/projects/',json_decode($project->images)[0])}}" alt="">
+                                <img class="img-fluid rounded lazyload" src="{{Storage::url($project->thumbnail)}}" loading="lazy" alt="">
                                 <div class="overlay">
                                     <a href="{{route('project-show',$project->slug)}}" class="view-icon">
                                         <i class="fa fa-eye fa-2x"></i>
