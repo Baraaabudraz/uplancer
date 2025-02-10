cdcce152edbb7e2a80542ae81aa35aec199eb3b0
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-10 09:51:24 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,6 +6,7 @@ use App\Models\Admin;
 use App\Models\Company;
 use App\Models\Trader;
 use App\Models\User;
+use App\Services\ImagesUploadService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -56,9 +57,10 @@ class ProfileController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id , ImagesUploadService $imagesUploadService)
     {
         $request->request->add(['id' => $id]);
+
         if (Auth::guard('web')->check()) {
             $request->validate([
                 'id' => 'required|integer|exists:users',
@@ -75,38 +77,40 @@ class ProfileController extends Controller
             session()->flash('message', 'تم تعديل بيانات المستخدم بنجاح');
             return redirect()->back();
 
-        } elseif (Auth::guard('admin')->check()) {
+        }
+        if (Auth::guard('admin')->check()) {
             $request->validate([
                 'id' => 'required|integer|exists:admins,id',
                 'name' => 'required|string|min:3|max:30,' . $id,
                 'email' => 'required|string|email|unique:admins,email,' . $id,
                 'password' => 'required|string',
                 'address' => 'required|string|min:3|max:20',
-                'mobile_num' => 'required|string|unique:admins,mobile_num,' . $id,
+                'mobile_number' => 'required|string|unique:admins,mobile_number,' . $id,
                 'gender' => 'required|string|in:Male,Female',
             ]);
             $data = $request->only([
                 'name', 'email', 'password', 'address', 'mobile_num', 'gender',
             ]);
-            $admin= Admin::query()->find($id);
+            $admin  = Admin::query()->find($id);
+
             if ($admin){
                 if ($request->hasFile('image')){
                     // إذا كان هناك صورة جديدة، احذف القديمة وقم بتحديث الصورة
-                    $imagePath = public_path('images/admin/' . $admin->image);
+                    $imagePath = public_path('storage/' . $admin->image);
                     if (file_exists($imagePath)){
                         unlink($imagePath);
                     }
-                    $newImage = $request->image->hashName();
-                    $request->image->move(public_path('images/admin'), $newImage);
-                    $data['image'] = $newImage;
+                    $image = $imagesUploadService->uploadImage($request, 'image','images/projects');
+                    $data['image'] = $image;
                 }else{
-                    // احتفظ بالصورة القديمة في حالة عدم تحميل صورة جديدة
                     $data['image']=$admin->image;
                 }
             }
 
             $data['password'] = Hash::make($request->get('password'));
+
             $isUpdated = $admin->update($data);
+
             if ($isUpdated) {
                 session()->flash('alert-type', 'alert-success');
                 session()->flash('message', 'تم تعديل  بيانات المستخدم بنجاح');
