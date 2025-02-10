2666b6fb0b5714e18e62cb8e9b6662406993b99e
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-10 09:55:41 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -121,66 +121,7 @@ class ProfileController extends Controller
                 return redirect()->back();
             }
 
-        } elseif (Auth::guard('company')->check()) {
-            $request->validate([
-                'id' => 'required|integer|exists:companies,id',
-                'name' => 'required',
-                'user_name' => 'required|string|regex:/^\S*$/|unique:companies,user_name,' . $id,
-                'password' => 'required|string',
-                'address' => 'required|string',
-                'phone' => 'required|numeric|string|unique:companies,phone,' . $id,
-            ]);
-            $data = $request->only([
-                'name', 'user_name', 'address', 'phone',
-            ]);
-            if ($request->hasFile('image')) {
-                $companyImage = $request->file('image');
-                $imageName = time() . '_' . $request->get('name') . '.' . $companyImage->getClientOriginalExtension();
-                $companyImage->move(public_path('images/companies'), $imageName);
-                $data['image'] = $imageName;
-            }
-            $data['password'] = Hash::make($request->get('password'));
-            $company = Company::query()->find($id)->update($data);
-            if ($company) {
-                session()->flash('alert-type', 'alert-success');
-                session()->flash('message', 'تم تعديل بيانات الشركة بنجاح');
-                return redirect()->back();
-            } else {
-                session()->flash('alert-type', 'alert-danger');
-                session()->flash('alert-type', 'لم يتم تعديل بيانات الشركة!');
-                return redirect()->back();
-            }
-
-        } elseif (Auth::guard('trader')->check()) {
-            $request->validate([
-                'id' => 'required|integer|exists:traders',
-                'name' => 'required|string|min:3|max:45',
-                'user_name' => 'required|unique:traders,user_name,' . $id,
-                'password'  =>'required|string|min:8|max:15',
-                'address' => 'required|string|max:30',
-                'store.*' => 'required|max:30',
-                'mobile_number' => 'required|numeric|string|unique:traders,mobile_number,' . $id,
-                'dinar_balance' => 'required|string',
-                'dollar_balance' => 'required|string',
-            ]);
-            $data = $request->only([
-                'name', 'user_name', 'address',
-                'mobile_number', 'dinar_balance', 'dollar_balance',
-            ]);
-            $data['store'] = implode(',', $request->get('store'));
-            $data['password']=Hash::make($request->password);
-            $trader = Trader::query()->find($id)->update($data);
-            if ($trader) {
-                session()->flash('alert-type', 'alert-success');
-                session()->flash('message', 'تم تعديل بيانات التاجر بنجاح');
-                return redirect()->back();
-            }else{
-                session()->flash('alert-type', 'alert-danger');
-                session()->flash('message', 'فشلت عملية تعديل بيانات التاجر !');
-                return redirect()->back();
-            }
         }
-
     }
 
     /**
