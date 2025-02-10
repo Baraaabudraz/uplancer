fcc6e26483cb7c35833efb74bf0b8bea75bb2ba3
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-10 14:14:31 +0200

    Create 2025_02_10_102438_add_slogan_to_settings_table.php

---
+++ database/migrations/2025_02_10_102438_add_slogan_to_settings_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->json('slogan')->after('name')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            //
+        });
+    }
+};
