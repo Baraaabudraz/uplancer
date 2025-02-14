2c5dccde26ab63138a20ec1e34611c459d0a32d5
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-14 15:47:12 +0200

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -198,7 +198,7 @@ class ProjectController extends Controller
             return ControllerHelper::generateResponse('error', trans('dashboard_trans.Failed to updated project'), 400);
         }
     }
-    
+
     /**
      * Remove the specified resource from storage.
      */
