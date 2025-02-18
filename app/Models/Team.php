0350ba891e122609a50f9a33c03e6671a732c3f7
Baraa Mohammad <102161963+Baraaabudraz@users.noreply.github.com>
2025-02-18 12:33:29 +0200

    Update Team.php

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Spatie\Translatable\HasTranslations;
 
 class Team extends Model
@@ -10,4 +11,9 @@ class Team extends Model
     use HasTranslations;
     protected $translatable  = ['name'  , 'position' ];
     protected $guarded = [];
+
+    public function role() : hasOne
+    {
+        return $this->hasOne(Role::class ,'id' , 'role_id');
+    }
 }
